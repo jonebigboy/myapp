f578650a632e0c947c41237a444e3d54dcfe139b
jonebigboy <975839449@qq.com>
2022-04-28 15:46:59 +0800

    up

---
+++ scripts/game_js_src2dis.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+JS_PATH=/home/jonebigboy/myapp/game/static/js/
+JS_PATH_SCR=${JS_PATH}src/
+JS_PATH_DIS=${JS_PATH}dis/
+
+find ${JS_PATH_SCR} -type f -name "*.js" | sort | xargs cat > ${JS_PATH_DIS}game.js
+
+
+
