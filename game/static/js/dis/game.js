4cd667ff1dd40d1de87e03ee27ac0e95b4f40c24
jonebigboy <975839449@qq.com>
2022-05-11 17:49:11 +0800

    acapp login

--- game/static/js/dis/game.js
+++ game/static/js/dis/game.js
@@ -577,9 +577,14 @@ class Settings{
     }
 
     start(){
-        this.getinfo();
-        this.add_listening_events();
+        if(this.platform==="WEB"){
+            this.getinfo_web();
+            this.add_listening_events();
+        }else{
+            this.getinfo_acapp();
+        }
     }
+
     
     add_listening_events(){
         let outer =this;
@@ -686,7 +691,35 @@ class Settings{
         });
     }
 
-    getinfo(){
+    acapp_login(appid,redirect_uri,scope,state){
+        let outer=this;
+        this.root.AcWingOS.api.oauth2.authorize(appid, redirect_uri, scope, state, function(resp){
+            console.log(resp);
+            if(resp.result==="success"){
+                outer.username=resp.username;
+                outer.photo=resp.photo;
+                outer.hide();
+                outer.root.menu.show();
+            }
+        
+        });
+    }
+
+    getinfo_acapp(){
+        let outer=this;
+        $.ajax({
+            url:"https://app2295.acapp.acwing.com.cn/settings/acwing/acapp/apply_code",
+            type:"GET",
+            success:function(resp){
+                
+                if(resp.result==="success"){
+                    outer.acapp_login(resp.appid,resp.redirect_uri,resp.scope,resp.state);
+                }
+            },
+        });
+    }
+
+    getinfo_web(){
         let outer=this;
 
         $.ajax({
--- game/urls/settings/acwing/index.py
+++ game/urls/settings/acwing/index.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from game.views.settings.acwing.web.apply_code import apply_code
-from game.views.settings.acwing.web.receive_code import receive_code
+from game.views.settings.acwing.web.apply_code import apply_code as web_ac
+from game.views.settings.acwing.web.receive_code import receive_code as web_rc
+from game.views.settings.acwing.acapp.apply_code import apply_code as acapp_ac
+from game.views.settings.acwing.acapp.receive_code import receive_code as acapp_rc
 
 urlpatterns=[
-    path("web/apply_code/",apply_code,name="settings_acwing_web_apply_code"),
-    path("web/receive_code/",receive_code,name="settings_acwing_web_receive_code"),
+    path("web/apply_code/",web_ac,name="settings_acwing_web_apply_code"),
+    path("web/receive_code/",web_rc,name="settings_acwing_web_receive_code"),
+    path("acapp/apply_code",acapp_ac,name="settings_acwing_acapp_apply_code"),
+    path("acapp/receive_code/",acapp_rc,name="settings_acwing_acapp_receive_code"),
 ]
